193c29d7074aa50b7cddb8f9148ef2bd3c3c2426
Gabriel Serrano <gjserrano@ksu.edu>
2017-09-19 22:18:00 -0500

    Add project files.

---
+++ KsuCis300_Lab_4/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace KsuCis300_Lab_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxOpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxSaveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxTextBox = new System.Windows.Forms.TextBox();
+            this.uxOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.uxSaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.encryptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxWithString = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxWithStringBuilder = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uxMenuStrip
+            // 
+            this.uxMenuStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.uxMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.encryptToolStripMenuItem});
+            this.uxMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.uxMenuStrip.Name = "uxMenuStrip";
+            this.uxMenuStrip.Size = new System.Drawing.Size(862, 42);
+            this.uxMenuStrip.TabIndex = 0;
+            this.uxMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uxOpenToolStripMenuItem,
+            this.uxSaveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 36);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // uxOpenToolStripMenuItem
+            // 
+            this.uxOpenToolStripMenuItem.Name = "uxOpenToolStripMenuItem";
+            this.uxOpenToolStripMenuItem.Size = new System.Drawing.Size(268, 38);
+            this.uxOpenToolStripMenuItem.Text = "Open...";
+            this.uxOpenToolStripMenuItem.Click += new System.EventHandler(this.uxOpenToolStripMenuItem_Click);
+            // 
+            // uxSaveAsToolStripMenuItem
+            // 
+            this.uxSaveAsToolStripMenuItem.Name = "uxSaveAsToolStripMenuItem";
+            this.uxSaveAsToolStripMenuItem.Size = new System.Drawing.Size(268, 38);
+            this.uxSaveAsToolStripMenuItem.Text = "Save As...";
+            this.uxSaveAsToolStripMenuItem.Click += new System.EventHandler(this.uxSaveAsToolStripMenuItem_Click);
+            // 
+            // uxTextBox
+            // 
+            this.uxTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxTextBox.Location = new System.Drawing.Point(31, 76);
+            this.uxTextBox.Multiline = true;
+            this.uxTextBox.Name = "uxTextBox";
+            this.uxTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.uxTextBox.Size = new System.Drawing.Size(780, 404);
+            this.uxTextBox.TabIndex = 1;
+            // 
+            // uxOpenFileDialog
+            // 
+            this.uxOpenFileDialog.FileName = "openFileDialog1";
+            // 
+            // encryptToolStripMenuItem
+            // 
+            this.encryptToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uxWithString,
+            this.uxWithStringBuilder});
+            this.encryptToolStripMenuItem.Name = "encryptToolStripMenuItem";
+            this.encryptToolStripMenuItem.Size = new System.Drawing.Size(106, 38);
+            this.encryptToolStripMenuItem.Text = "Encrypt";
+            // 
+            // uxWithString
+            // 
+            this.uxWithString.Name = "uxWithString";
+            this.uxWithString.Size = new System.Drawing.Size(323, 38);
+            this.uxWithString.Text = "With String...";
+            this.uxWithString.Click += new System.EventHandler(this.uxWithString_Click);
+            // 
+            // uxWithStringBuilder
+            // 
+            this.uxWithStringBuilder.Name = "uxWithStringBuilder";
+            this.uxWithStringBuilder.Size = new System.Drawing.Size(323, 38);
+            this.uxWithStringBuilder.Text = "With StringBuilder...";
+            this.uxWithStringBuilder.Click += new System.EventHandler(this.uxWithStringBuilder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 509);
+            this.Controls.Add(this.uxTextBox);
+            this.Controls.Add(this.uxMenuStrip);
+            this.MainMenuStrip = this.uxMenuStrip;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.uxMenuStrip.ResumeLayout(false);
+            this.uxMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip uxMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uxOpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uxSaveAsToolStripMenuItem;
+        private System.Windows.Forms.TextBox uxTextBox;
+        private System.Windows.Forms.OpenFileDialog uxOpenFileDialog;
+        private System.Windows.Forms.SaveFileDialog uxSaveFileDialog;
+        private System.Windows.Forms.ToolStripMenuItem encryptToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uxWithString;
+        private System.Windows.Forms.ToolStripMenuItem uxWithStringBuilder;
+    }
+}
+
---
+++ KsuCis300_Lab_4/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KsuCis300_Lab_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void uxOpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(uxOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    uxTextBox.Text = File.ReadAllText(uxOpenFileDialog.FileName);
+                }
+                catch(Exception ex)
+                {
+                    DisplayError(ex);
+                }
+            }
+        }
+
+        private void uxSaveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.WriteAllText(uxSaveFileDialog.FileName, uxTextBox.Text);
+            }
+            catch(Exception ex)
+            {
+                DisplayError(ex);
+            }
+        }
+
+        private void DisplayError(Exception ex)
+        {
+            MessageBox.Show("The following error occured: " + ex);
+        }
+
+        /// <summary>
+        /// Rotates the given character c n positions through the alphabet whose first
+        /// letter is firstLetter and whose number of letters is alphabetLen. alphabetLen
+        /// must be positive.
+        /// </summary>
+        /// <param name="c">The character to rotate.</param>
+        /// <param name="n">The number of positions to rotate c.</param>
+        /// <param name="firstLetter">The first letter of the alphabet.</param>
+        /// <param name="alphabetLen">The number of letters in the alphabet.</param>
+        /// <returns>The result of the rotation.</returns>
+        private char Rotate(char c, int n, char firstLetter, int alphabetLen)
+        {
+            return (char)(firstLetter + (c - firstLetter + n) % alphabetLen);
+        }
+
+        private char Encrypt(char inputC)
+        {
+            char returnChar = 'a';
+
+            if(inputC >= 'a' && inputC <= 'z')
+            {
+                returnChar = Rotate(inputC, 13, 'a', 26);
+            }
+
+            else if(inputC >= 'A' && inputC <= 'Z')
+            {
+                returnChar = Rotate(inputC, 13, 'A', 26);
+            }
+
+            else
+            {
+                returnChar = inputC;
+            }
+
+            return returnChar;
+        }
+
+        private void uxWithString_Click(object sender, EventArgs e)
+        {
+            string text = uxTextBox.Text;
+            string emptyString = "";
+
+            foreach(char ch in text)
+            {
+                emptyString += Encrypt(ch);
+            }
+
+            uxTextBox.Text = emptyString;
+        }
+
+        private void uxWithStringBuilder_Click(object sender, EventArgs e)
+        {
+            StringBuilder build = new StringBuilder();
+
+            string text = uxTextBox.Text;
+
+            foreach(char ch in text)
+            {
+                build.Append(Encrypt(ch));
+            }
+
+            uxTextBox.Text = build.ToString();
+        }
+    }
+}
